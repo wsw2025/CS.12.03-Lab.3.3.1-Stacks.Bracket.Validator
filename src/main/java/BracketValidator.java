194608c8ede2ad73b4c7996ee7f6f80ca57fd2be
Adam <adam.ewing.nz@gmail.com>
2024-02-29 09:30:28 +0800

    First commit.

--- src/main/java/BracketValidator.java
+++ src/main/java/BracketValidator.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class BracketValidator {
+import java.util.Stack;
+
+public class BracketValidator {
+
+    // Method called checkValidParentheses that returns a boolean indicating whether a string of brackets is valid or not.
+    public static boolean checkValidParentheses(String expression) {
+
+        // Obtain an array of the brackets (each element is a single bracket).
+
+        // Create a stack.
+
+        // Iterate over the array of brackets.
+
+            // Obtain the reversed version of bracket.
+
+            // Check if the bracket is an opening bracket. If it is, push it onto the stack.
+
+            // If the stack is empty or if the popped bracket is not equal to the reverse bracket of the current bracket, return false as this must mean that the expression is invalid.
+
+        // If the stack is empty after we have finished iterating over the array of brackets, then return true as this must mean that the expression is valid. If the stack is not empty, this must mean that the expression is invalid.
+        return false;
+
+    }
+
+    // Private helper method called isLeftBracket that returns a boolean value to indicate whether the bracket is a left (opening) bracket.
+    private static boolean isLeftBracket(String bracket) {
+
+        return false;
+
+    }
+
+    // Private helper method called getReversedBracket that returns a String which is the reverse version of the bracket passed to the method.
+    private static String getReversedBracket(String bracket) {
+
+        return null;
+
+    }
 }
