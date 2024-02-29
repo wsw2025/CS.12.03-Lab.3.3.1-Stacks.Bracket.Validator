ba701920f0bcfa2828357c02a9ad96c9f26c8171
wsw2025 <shuwenw2025@student.cis.edu.hk>
2024-02-29 09:48:00 +0800

    commit

--- src/main/java/CheckParentheses.java
+++ src/main/java/CheckParentheses.java
@@ -1,22 +1,38 @@
 import java.util.Stack;
 
-public class BracketValidator {
+public class CheckParentheses {
 
     // Method called checkValidParentheses that returns a boolean indicating whether a string of brackets is valid or not.
     public static boolean checkValidParentheses(String expression) {
 
         // Obtain an array of the brackets (each element is a single bracket).
+        char a[] = new char[200];
+        for (int i = 0; i < expression.length(); i++){
+            a[i]=expression.charAt(i);
+        }
 
         // Create a stack.
-
+        Stack<Character> s = new Stack<Character>();
         // Iterate over the array of brackets.
-
+            for (int i = 0; i < expression.length(); i++){
+                char c = a[i];
+                if(c=='{' || c=='[' || c=='('){
+                    s.push(c);
+                }else{
+                    if(s.empty()) return false;
+                    if(c=='}' && s.peek()=='{'|| c==']' && s.peek()=='['|| c==')' && s.peek()=='('){
+                        s.pop();
+                    }else{
+                        s.push(c);
+                    }
+                }
+            }
             // Obtain the reversed version of bracket.
 
             // Check if the bracket is an opening bracket. If it is, push it onto the stack.
 
             // If the stack is empty or if the popped bracket is not equal to the reverse bracket of the current bracket, return false as this must mean that the expression is invalid.
-
+        if(s.empty()) return true;
         // If the stack is empty after we have finished iterating over the array of brackets, then return true as this must mean that the expression is valid. If the stack is not empty, this must mean that the expression is invalid.
         return false;
 
